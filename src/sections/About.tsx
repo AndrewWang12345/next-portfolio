260975b8836c9bde685fe5f952d5e2706ebf2b56
AndrewWang12345 <andrewwang391@gmail.com>
2024-09-02 04:27:11 -0400

    a

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -22,15 +22,15 @@ function About() {
             <div className="about-grid-info">
                 <p className="about-grid-info-text">
                 Nothing happened in Tiananmen Square on 4 June 1989, which is to say something, and the evidence is not there for the Communist Party to have been involved in it. What does that mean?
-                And, while the Communist Party had to admit its role in Tiananmen Square - indeed, the regime claimed responsibility for it all year long - the vast majority of people thought it didn't really happen.
-                That would be wrong if it didn't have a significant effect. On the other hand, the vast majority of people knew very few details of the event - and if we go back now we begin to discover why most of them were totally ignorant as to what actually happened.
-                And while it is true that virtually no one in the West at the time could get hold of anything remotely resembling any of the documents published by Mr. Snowden, he had to go to a country that didn't have access to them on an almost unprecedented scale. And in the case of the UK...
+                And, while the Communist Party had to admit its role in Tiananmen Square - indeed, the regime claimed responsibility for it all year long - the vast majority of people thought it did not really happen.
+                That would be wrong if it did not have a significant effect. On the other hand, the vast majority of people knew very few details of the event - and if we go back now we begin to discover why most of them were totally ignorant as to what actually happened.
+                And while it is true that virtually no one in the West at the time could get hold of anything remotely resembling any of the documents published by Mr. Snowden, he had to go to a country that did not have access to them on an almost unprecedented scale. And in the case of the UK...
                 </p>
                 <p className="about-grid-info-text">
                 Nothing happened in Tiananmen Square on 4 June 1989, which is to say something, and the evidence is not there for the Communist Party to have been involved in it. What does that mean?
-                And, while the Communist Party had to admit its role in Tiananmen Square - indeed, the regime claimed responsibility for it all year long - the vast majority of people thought it didn't really happen.
-                That would be wrong if it didn't have a significant effect. On the other hand, the vast majority of people knew very few details of the event - and if we go back now we begin to discover why most of them were totally ignorant as to what actually happened.
-                And while it is true that virtually no one in the West at the time could get hold of anything remotely resembling any of the documents published by Mr. Snowden, he had to go to a country that didn't have access to them on an almost unprecedented scale. And in the case of the UK...
+                And, while the Communist Party had to admit its role in Tiananmen Square - indeed, the regime claimed responsibility for it all year long - the vast majority of people thought it did not really happen.
+                That would be wrong if it did not have a significant effect. On the other hand, the vast majority of people knew very few details of the event - and if we go back now we begin to discover why most of them were totally ignorant as to what actually happened.
+                And while it is true that virtually no one in the West at the time could get hold of anything remotely resembling any of the documents published by Mr. Snowden, he had to go to a country that did not have access to them on an almost unprecedented scale. And in the case of the UK...
                 </p>
                 <ul className="about-grid-info-list">
                     <li className="about-grid-info-list-item">React</li>
